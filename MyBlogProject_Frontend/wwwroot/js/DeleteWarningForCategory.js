125e2b8db2acda8df2d5510556d119a8c8b8b0bb
Tugrul <Tugrul@DESKTOP-D80M3PV>
2023-03-22 16:34:47 +0300

    CategoryDelete is Done

---
+++ MyBlogProject_Frontend/wwwroot/js/DeleteWarningForCategory.js
@@ -0,0 +1,63 @@
+﻿$(document).ready(function () {
+
+    $("#DeleteCategory").click(function (e) {
+
+        const dataId = $("#DeleteCategory").attr("span");
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                $.ajax({
+                    url: "/Category/Delete",
+                    type: "Post",
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    data: JSON.stringify(dataId),
+                    success: function (response) {
+                        if (response.isSuccess) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your File has been deleted',
+                                'success'
+                            )
+                            setTimeout(function () {
+                                window.location.href = "/Category/Index"
+
+                            }, 1500)
+                        }
+                        else {
+                            swal.fire(
+                                'Deleted!',
+                                'Your file has not been deleted',
+                                'error'
+                            )
+                        }
+
+                    },
+                    error: function (response) {
+
+                    }
+
+
+                });
+
+            }
+        });      
+
+
+
+    });
+
+
+});
+
+

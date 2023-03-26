412f115fba354431b73f8699c91274ba94566bd6
Tugrul <Tugrul@DESKTOP-D80M3PV>
2023-03-26 20:11:49 +0300

    ArticleSweetAlerts are Done

---
+++ MyBlogProject_Frontend/wwwroot/js/ArticleUpdateWarning.js
@@ -0,0 +1,69 @@
+﻿
+
+$(document).ready(function () {
+
+    $("#UpdateButon").click(function (e) {
+
+        var dataform =
+        {
+            ID: $("#ArticleID").val(),
+            Title: $("#ArticleTitle").val(),
+            Content: $("#ArticleContent").val(),
+            CategoryId: $("#ArticleCategoryID").val()
+        }
+        
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Emin misiniz?',
+            text: "Bu işlemi geri alamassınız!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Güncelle!',
+            cancelButtonText: "İptal"
+        }).then((result) => {
+
+            if (result.isConfirmed) {
+
+                $.ajax({
+                    url: "/Article/Update",
+                    type: "post",
+                    contentType: 'application/json',
+                    datatype: "json",
+                    data: JSON.stringify(dataform),
+
+                    success: function (response) {
+                        if (response.isSuccess) {
+                            Swal.fire(
+                                'Güncelle!',
+                                'Makale Başarıyla Güncellendi',
+                                'success'
+                            )
+                            setTimeout(function () {
+                                window.location.href = "/Article/Index"
+                            }, 1500)
+                        }
+                        else {
+                            Swal.fire(
+                                'Güncellenemedi!',
+                                'Makale Güncellenirken Hata Oluştu',
+                                'error'
+
+                            )
+                        }
+
+                    },
+                    error: function (response) {
+
+                    }
+
+                });
+            }
+        });
+
+
+    });
+
+});
---
+++ MyBlogProject_Frontend/wwwroot/js/CategoryAddWithAjax.js
@@ -0,0 +1,67 @@
+﻿
+
+
+$(document).ready(function () {
+
+
+    $("#btnSave").click(function (event) {
+
+        
+
+        event.preventDefault();
+
+        var addUrl = app.Urls.categoryAddUrl;
+        var redirectUrl = app.Urls.articleAddUrl;
+
+        var categoryAddDto = {
+            Name: $("input[id=categoryName]").val()
+        }
+
+        var jsonData = JSON.stringify(categoryAddDto);
+
+        debugger;
+
+
+
+        $.ajax({
+            url: addUrl,
+            type: "Post",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: jsonData,
+           
+
+                success: function (response) {
+                    if (response.isSuccess) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Kategori Başarıyla Eklendi',
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                        setTimeout(function () {
+                            window.location.href = redirectUrl;
+                        }, 1700)
+                    }
+                    else {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'error',
+                            title: 'Kategori Eklenirken Bir Hata Oluştu',
+                            showConfirmButton: false,
+                            timer: 2000
+                        })
+                    }
+
+                },
+                error: function (response) {
+
+                }
+        
+
+        });
+
+    });
+
+});

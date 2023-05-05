5204af2dcf045fb5e868475f424106c6d0e096d1
Tugrul <Tugrul@DESKTOP-D80M3PV>
2023-05-05 22:35:34 +0300

    MessageBox BackEnd is Done

---
+++ MyBlogProject_Frontend/Areas/Admin/Controllers/MessageBoxController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using MyBlogProject_Frontend.Areas.Admin.AttributeFilters;
+using MyBlogProject_Frontend.Areas.Admin.Models.DTOs.Article;
+using MyBlogProject_Frontend.Models.Context;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace MyBlogProject_Frontend.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class MessageBoxController : Controller
+	{
+        private readonly ConsumerDbContext _consumerDbContext;
+        public MessageBoxController(ConsumerDbContext consumerDbContext)
+		{
+		      this._consumerDbContext = consumerDbContext;
+		}
+
+		public IActionResult Index()
+		{
+			var Contacts = _consumerDbContext.Contact.ToList();
+
+			return View(Contacts);
+		}
+
+	
+		public IActionResult Delete([FromBody] int id)
+		{
+			var data = _consumerDbContext.Contact.SingleOrDefault(x => x.ID == id);
+
+			_consumerDbContext.Remove(data);
+
+			if (_consumerDbContext.SaveChanges() >= 0) 
+			{
+				return Json(new { isSuccess = true });
+			}
+
+			return Json(new { isSuccess = false });			
+
+		}
+
+		public IActionResult Details(int Id)
+		{
+			var data = _consumerDbContext.Contact.SingleOrDefault(y=>y.ID== Id);
+
+			return View(data);
+		}
+
+	}
+}
---
+++ MyBlogProject_Frontend/wwwroot/js/ContactDeleteWarning.js
@@ -0,0 +1,63 @@
+﻿$(document).ready(function () {
+    $(".ContactDeleteButon").click(function (e) {
+
+
+        const dataId = $(this).attr("x");
+
+        e.preventDefault();
+
+        Swal.fire({
+            title: 'Emin Misiniz?',
+            text: 'Bu işlemi Geri Alamassınız',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil!',
+            cancelButtonText: 'İptal',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "/admin/MessageBox/Delete",
+                    type: "Post",
+                    contentType: 'application/json',
+                    dataType: 'json',
+                    data: JSON.stringify(dataId),
+                    success: function (response)
+                    {
+                        if (response.isSuccess) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Mesaj Başarıyla Silindi',
+                                'success'
+                            )
+                            setTimeout(function () {
+                                window.location.href = "/admin/MessageBox/Index"
+
+                            }, 1500)
+
+                        }
+                        else
+                        {
+                            Swal.fire(
+                                'Deleted!',
+                                'Mesaj Silinirken Bir Hata Oluştu!',
+                                'error'
+                            )
+                        } 
+
+                    }
+                });
+            }
+            else
+            {
+                Swal.fire(
+                    'Deleted!',
+                    'Mesaj Silinirken Bir Hata Oluştu!',
+                    'error'
+                )
+            }
+        });
+    });
+
+});
